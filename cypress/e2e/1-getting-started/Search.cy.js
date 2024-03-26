a9dd50def02e99f4172fe28de61c25e31bcca628
yazeedk <36983323+yazeedk@users.noreply.github.com>
2024-03-26 11:32:53 +0200

    Update Search.cy.js

--- cypress/e2e/1-getting-started/Search.cy.js
+++ cypress/e2e/1-getting-started/Search.cy.js
@@ -5,91 +5,121 @@ describe('Search Bar Functionality', () => {
     });
     
     afterEach(() => {
-        cy.wait(1500);
+        cy.wait(1000);
     });
 
     after(() => {
         cy.get('.search-bar').clear();
     });
     
+
     it('Search with full company name', () => {
         cy.get('input[class="search-bar"]').type('Bisan Systems');
-        cy.get('.last-search-and-results .item-text').contains('Bisan Systems').should('be.visible');
-    });
-
-    it('Search with first letter of company name', () => {
-        cy.get('input[class="search-bar"]').type('A');
-        cy.get('.last-search-and-results .item-text').contains('Al Andalus Software Development').should('be.visible');
+        cy.get('.company-name').should('contain', 'Bisan Systems');
     });
     
+    it('Search with first letter of company name', () => {
+        cy.get('input[class="search-bar"]').type('A');
+        cy.get('.last-search-and-results .item-text').each(($element) => {
+            cy.wrap($element).invoke('text').should('match', /^A/);    
+        });
+    });
+
     it('Search with non-existing company', () => {
         cy.get('input[class="search-bar"]').type('Meta');
-        //cy.get('.last-search-and-results').should('not.contain', '.item-text', 'Meta');
+        cy.get('.company-name').contains('Al Andalus Software Development').should('not.exist');
     });
 
     it('Search for the company in capital letters', () => {
         cy.get('input[class="search-bar"]').type('ASAL');
-        cy.get('.last-search-and-results .item-text').contains('ASAL Technologies').should('be.visible');
+        cy.get('.company-name').contains('ASAL Technologies').should('be.visible');
     });
 
     it('Search for the company in small letters', () => {
         cy.get('input[class="search-bar"]').type('asal');
-        cy.get('.last-search-and-results .item-text').contains('ASAL Technologies').should('be.visible');
-        });
+        cy.get('.company-name').contains('ASAL Technologies').should('be.visible');
+    });
 
     it('Search for the company using its first name', () => {
         cy.get('input[class="search-bar"]').type('EXALT');
-        cy.get('.last-search-and-results .item-text').contains('EXALT Technologies Ltd.').should('be.visible');
+        cy.get('.company-name').contains('EXALT').should('be.visible');
     });
 
     it('Search for a company using a letter in the middle of its name', () => {
         cy.get('input[class="search-bar"]').type('A');
-        cy.get('.last-search-and-results .item-text').contains('Advice Technologies').should('be.visible');
+        cy.get('.company-name').contains('Advice Technologies').should('be.visible');
     });
 
     it('Search for a company using his first three letters', () => {
         cy.get('input[class="search-bar"]').type('Aux');
-        cy.get('.last-search-and-results .item-text').contains('Auxilium Technology').should('be.visible');
+        cy.get('.company-name').contains('Auxilium Technology').should('be.visible');
     });
 
     it('Type numbers on the search bar', () => {
         cy.get('input[class="search-bar"]').type('8');
-        cy.get('.last-search-and-results .item-text').should('not.exist');
+        cy.get('.company-name').should('not.exist');
     });
 
     it('Type special characters on the search bar', () => {
         cy.get('input[class="search-bar"]').type('%^');
-        cy.get('.last-search-and-results .item-text').should('not.exist');
+        cy.get('.company-name').should('not.exist');
     });
 
     it('Search for a company using the city it is located in', () => {
-        cy.get('input[class="search-bar"]').type('Nablus').type('{enter}');;
-        cy.get('.company-slogan').should('contain', 'Nablus')
+        cy.get('input[class="search-bar"]').type('Nablus').type('{enter}');
+        cy.get('.company-slogan').should('contain', 'Nablus');
     });
 
     it('Search for a company using his topic', () => {
-        cy.get('input[class="search-bar"]').type('Mobile').type('{enter}');;
-        //cy.get('.slider-element.purple').should('contain', 'Mobile')    
+        cy.get('input[class="search-bar"]').type('Mobile').type('{enter}');
+        cy.get('.slider-element.mobile').should('contain', 'Mobile');   
     });
 
-    
     it('Open a topic from last search menu', () => {
-        cy.get('input[class="search-bar"]').type('front').type('{enter}');;
+        cy.get('input[class="search-bar"]').type('front').type('{enter}');
         cy.get('#root > div > div.navbar > div.search-bar-container > input').click();
         cy.get('.item-text-button').click();
     });
 
     it('Remove a topic from last search', () => {
-        cy.get('input[class="search-bar"]').type('front').type('{enter}');;
+        cy.get('input[class="search-bar"]').type('front').type('{enter}');
         cy.get('#root > div > div.navbar > div.search-bar-container > input').click();
         cy.get('button.item-text-button').click();
 
     });
 
     it('Remove all of topics from last search', () => {
-        cy.get('input[class="search-bar"]').type('front').type('{enter}');;
+        cy.get('input[class="search-bar"]').type('front').type('{enter}');
         cy.get('#root > div > div.navbar > div.search-bar-container > input').click();
         cy.get('.header-last > .remove-button').click();
     });
 
+    it('Search with full company name for suggestion', () => {
+        cy.get('input[class="search-bar"]').type('Bisan Systems');
+        cy.get('.last-search-and-results .item-text').contains('Bisan Systems').should('be.visible');
+    });
+
+    it('Search with first letter of company name for suggestion', () => {
+        cy.get('input[class="search-bar"]').type('A');
+        cy.get('.last-search-and-results .item-text').each(($element) => {
+            cy.wrap($element).invoke('text').should('match', /^A/);
+        });
+    });
+    
+    it('Search for a company using his topic for suggestion', () => {
+        cy.get('input[class="search-bar"]').type('Mobile').type('{enter}');
+        cy.get('#root > div > div.navbar > div.search-bar-container > input').click();
+        cy.get('.last-search-and-results .item-text').should('contain', 'Mobile');   
+    });
+
+    it('Search for a company using his sub topic for suggestion', () => {
+        cy.get('input[class="search-bar"]').type('MySQL').type('{enter}');
+        cy.get('#root > div > div.navbar > div.search-bar-container > input').click();
+        cy.get('.last-search-and-results .item-text').should('contain', 'MySQL');   
+    });
+
+    it('Search for a company using his city for suggestion', () => {
+        cy.get('input[class="search-bar"]').type('He');
+        cy.get('.item-text').should('contain', 'Hebron');
+    });
 });
